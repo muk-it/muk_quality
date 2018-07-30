331d51b23f735c8ee731c8c1492546674a16fb66
MuK IT GmbH <mathias.markl@mukit.at>
2018-07-30 23:42:44 +0000

    publish muk_quality_docs_dms - 11.0

--- muk_quality_docs_dms/__manifest__.py
+++ muk_quality_docs_dms/__manifest__.py
@@ -20,7 +20,7 @@
 {
     "name": "MuK QMS Documents DMS Support",
     "summary": """Quality Management System DMS Support""",
-    "version": "11.0.1.0.3",
+    "version": "11.0.1.0.4",
     "author": "MuK IT",
     "category": "Document Management",
     "license": "AGPL-3",
